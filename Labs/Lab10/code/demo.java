28c6dfaa31c4d35ab44fa4d08a5d14784f115a1a
pattonsgirl <pattonsgirl@gmail.com>
2022-11-08 10:36:45 -0500

    mass push

--- Labs/Lab10/code/demo.java
+++ Labs/Lab10/code/demo.java
@@ -1,6 +1,6 @@
 public class demo {
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
+		System.out.println("Hello world from Java!");
 		System.out.println("My new features!");
 	}
 }
---
+++ Labs/Lab10/code/demo.py
@@ -0,0 +1,3 @@
+#! /usr/bin/python3
+
+print("Hello World from Python 3")
