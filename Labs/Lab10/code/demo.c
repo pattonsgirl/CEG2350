697cd364842a4e1d595a7396819359d0e6003d7e
Kayleigh Duncan <pattonsgirl@gmail.com>
2022-11-10 15:55:00 +0000

    class demos 11/10

--- Labs/Lab10/code/demo.c
+++ Labs/Lab10/code/demo.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
 int main() {
+	static float GRAVITY = 9.8f;
+	// code for class
+	// adding another variable
+	int mynum = 42;
+
 	printf("Hello World form C!\n");
+	printf("The value of GRAVITY is %f\n", GRAVITY);
+	printf("The value of my variable is %d\n", mynum);
 	return 0;
 }
