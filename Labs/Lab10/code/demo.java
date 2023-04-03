6f4978f24a2fd49d3786bbcf76d9bb243ee2d9d8
Kayleigh Duncanwq <pattonsgirl@gmail.com>
2023-04-03 16:45:18 -0400

    hello world

--- Labs/Lab10/code/demo.java
+++ Labs/Lab10/code/demo.java
@@ -2,5 +2,6 @@ public class demo {
 	public static void main(String[] args) {
 		System.out.println("Hello world from Java!");
 		System.out.println("My new features!");
+		System.out.println("So happy!!!");
 	}
 }
