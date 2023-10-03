fc98b3c1e09735ac2f6b8bbaf7b0f458e316c2b5
Kayleigh Duncan <pattonsgirl@gmail.com>
2023-10-03 13:51:29 +0000

    bug fixed in adder and added printHelp function

--- scripts/calc.sh
+++ scripts/calc.sh
@@ -1,11 +1,20 @@
+# help function
+printHelp(){
+	echo "Usage guide"
+	echo " Call with calc.sh followed by calculator option (add or subtract) and two numbers"
+	echo 'calc.sh add 4 6'
+}
+
 add() {
 	echo "Add function called"
-	sum=( $1 + $2 )
+	sum=$(( $1 + $2 ))
 	echo "$1 and $2 added = $sum"
 }
 
 subtract() {
 	echo "Subtract function called"
+	result=$(( $1 - $2 ))
+	echo "$2 subtracted from $1 = $result" 
 }
 
 #add
@@ -15,8 +24,11 @@ subtract() {
 # have user give mode add / subtract
 if [[ $1 == "add" ]]; then
 	add $2 $3
+	# echo ( $2 + $3 )
 elif [[ $1 == "subtract" ]]; then
 	subtract $2 $3
+elif [[ $1 == "help" ]]; then
+	printHelp
 else 
 	echo "Bad option"
 fi
