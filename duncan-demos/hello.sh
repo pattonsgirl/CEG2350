fcad2373f6062b731fdb77f1c1498a090f90e832
Kayleigh Duncan <pattonsgirl@gmail.com>
2025-09-11 14:38:57 +0000

    read and echo operational

--- duncan-demos/hello.sh
+++ duncan-demos/hello.sh
@@ -1,3 +1,18 @@
 #! /usr/bin/bash
 
 echo "Hello world"
+
+# printf (I would get no newline)
+
+
+# TODO:
+var=""
+
+# read in text
+read var
+
+# print out said text
+echo $var
+
+# compare that text with a special word
+# loop until done
