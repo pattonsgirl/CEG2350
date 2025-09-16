6a95825bae54c4af25220c371b0565b1f92aa741
Kayleigh Duncan <pattonsgirl@gmail.com>
2025-09-16 11:26:54 -0400

    getting a random word

--- duncan-demos/hello.sh
+++ duncan-demos/hello.sh
@@ -19,4 +19,10 @@ if [[ $var = "test" ]]; then
 	echo "It's a match"
 fi
 
+# grab a random word
+words=($(grep '^\w\w\w\w\w$' /usr/share/dict/words | tr '[a-z]' '[A-Z]'))
+actual=${words[$[$RANDOM % ${#words[@]}]]}
+echo $words
+echo "Actual word was $actual"
+
 # loop until done
