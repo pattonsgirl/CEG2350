e1f384df63f1a71c747d5e5a9e0cebfebcad0ba1
Kayleigh Duncan <pattonsgirl@gmail.com>
2025-09-16 11:35:19 -0400

    checks 10 attempts.  TODO: user needs to input next attempt.

--- duncan-demos/hello.sh
+++ duncan-demos/hello.sh
@@ -16,11 +16,19 @@ echo $var
 
 words=($(grep '^\w\w\w\w\w$' /usr/share/dict/words | tr '[a-z]' '[A-Z]'))
 actual=${words[$[$RANDOM % ${#words[@]}]]}
+attempts=10
+attempt=0
 
-# compare a random word
-if [[ $var = $actual ]]; then
-	echo "It's a match"
-fi
+# loop for a number of attempts
+while [[ attempt -lt attempts ]]
+do
+	# compare a random word
+	if [[ $var = $actual ]]; then
+		echo "It's a match"
+	else
+		echo "Wrong!"
+		((attempt++))
+	fi
+done
 
 echo "Out of attempts, word was $actual"
-# loop until done
