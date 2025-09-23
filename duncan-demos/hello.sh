2ddf96775514cd1d0bff8167c3e705f695028766
Kayleigh Duncan <pattonsgirl@gmail.com>
2025-09-23 13:55:45 +0000

    did a demo of function

--- duncan-demos/hello.sh
+++ duncan-demos/hello.sh
@@ -2,6 +2,24 @@
 
 echo "Hello world"
 
+
+# every arg passed
+echo $@
+# total number of args
+echo $#
+
+echo "First arg to script is $1"
+
+demo () {
+	#arg1=$1
+	echo "In demo function, first arg to function is $1"
+	echo $1 > file.txt
+	return 42
+}
+demo "sleepin with the fishes"
+# ???
+echo $?
+
 # printf (I would get no newline)
 
 
