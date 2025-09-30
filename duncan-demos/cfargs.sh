9d8a1c3a8fca4a98ef5c5b45b6017c74b94d03b7
Kayleigh Duncan <pattonsgirl@gmail.com>
2025-09-30 14:50:24 +0000

    create files sample files

---
+++ duncan-demos/cfargs.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# The following script reads 3 arguments
+# Number of files
+# Type of mistake: "spaces" or "spelling"
+# Where to create files (in what folder)
+
+# Usage:
+# cfargs.sh 5 spaces .
+
+# Problems: 
+# order matters.  Entering an argument in wrong order will lead to disaster
+
+echo "$1 files with added mistake of $2 in directory $3"
+
+# if arguments are empty, abort program
+# TODO: did we need to check all 3?
+if [[ -z $1 ]] || [[ -z $2 ]] || [[ -z $3 ]]; then
+	echo "No files for you"
+	exit
+fi
+
+# TODO: check directory existence, stored in arg 3
+
+#for i in {1..$1}
+for (( i=0; i<=$1; i++ ))
+do
+	echo $i
+	if [[ $2 == "spaces" ]]; then
+		touch $3/"$i file name.txt"
+	elif [[ $2 == "spelling" ]]; then
+		touch $3/"$i-pic.jgp"
+	else
+		echo "Bad: $2"
+	fi
+done
---
+++ duncan-demos/cfgetopts.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+# function usageGuide prints usage guide on -h (help) option
+# could use if bad options provided as well
+usageGuide() {
+	echo "Usage guide for file creator"
+	echo "Sample: cfgetopts.sh -n 5 -t spelling ."
+	printf "\t-n number of files\n"
+	printf "\t-t type of error - spaces or spelling\n"
+	echo "final arg = directory to create files"
+}
+
+# :hn:t:
+# h is an option, does not require an arg (thus h)
+# n is an option, does require an arg (thus n:)
+# t is an option, does require an arg (thus t:)
+# The leading colon:  switches getopts to "silent error reporting mode"
+# So that you can handle errors yourself (think you handle Exceptions)
+while getopts ":hn:t:" opt; do
+	#echo "$OPTARG is currently stored in OPTARG"
+	#echo "$OPTIND is the index point in OPTIND"
+	case $opt in
+	h)
+		usageGuide
+		exit
+		;;
+	n)	#echo $OPTARG
+		num=$OPTARG
+		;;
+	t) 	
+		error=$OPTARG
+		;;
+	\?)
+      		echo "Invalid option: -$OPTARG" >&2
+      		;;
+	:)
+      		# this is surprisingly useless.  Take out the arg for -n.
+		# -t will then be read as the argument
+		# Try it: cfgetopts.sh -n
+		# Try it: cfgetopts.sh -n 5 -t
+		echo "Option -$OPTARG requires an argument." >&2
+      		exit 1
+      		;;
+	esac
+done
+
+# TODO: before echoing, check that both variables have contents.  
+# If no contents, print usage guide and exit
+
+# getopts has keep a running index of options checked so far
+# move to end of scanned options - in this case with expected usage
+# what "remains" is the folder specified
+shift $((OPTIND-1))
+echo $1
+
+echo "The user wants $num files"
+echo "File names should have $error errors"
+echo "Files should be created in $@"
+
+if [[ -d $@ ]];then
+	echo "Directory $@ exists"
+else
+	echo "Directory $@ does not exist bye"
+	usageGuide
+	exit	
+fi
+
+for (( i=1; i<=$num; i++ ))
+do
+	echo $i
+	if [[ $error == "spaces" ]]; then
+		touch $@/"$i file name.txt"
+	elif [[ $error == "spelling" ]]; then
+		touch $@/"$i-pic.jgp"
+	else
+		echo "Bad: $error"
+	fi
+done
+
