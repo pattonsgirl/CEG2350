9333dd3fcb7ecee2cf8c88ceaaf3e77f01000c30
Kayleigh Duncan <kayleigh.duncan@wright.edu>
2026-02-09 22:56:27 +0000

    won't make news files unless they don't exist

--- duncan-demos/file-creator.sh
+++ duncan-demos/file-creator.sh
@@ -8,10 +8,23 @@ help_function () {
         echo "Sample usage: file-creator.sh 55 .txt"
 }
 
+create () {
+	#how may, what base
+	for ((i = 1 ; i <= $1 ; i++)); do
+  		#echo "Counter: $i"
+		if [ -f file-$i$2 ]; then
+			echo "File already exists, not making"
+		else 
+			touch file-$i$2
+		fi
+	done
+}
+
 # read in two args - one tht defines how many, another base name
 
 # which args will hold which values?
 # check use gave me two args
+# num_files=$1
 if [ $# -eq 2 ]; then
 	# check first arg is a num
 	if [ $1 -gt 0 ]; then
@@ -21,11 +34,13 @@ if [ $# -eq 2 ]; then
 	fi	
 
 	# check string for arg 2
+	# check to see if the string given begins with a .
 	if [ -n $2 ]; then
 		echo "Heard, base name is $2"
 	else
 		help_function $2
 	fi
+	create $1 $2
 else 
 	echo "I said only two only args.  Bye."
 	exit
