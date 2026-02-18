f4fd01e271e5571da8ce9540999692f3ae40e1e4
Kayleigh Duncan <kayleigh.duncan@wright.edu>
2026-02-18 21:57:06 +0000

    starting a stolen getopts struct

--- duncan-demos/getopts.sh
+++ duncan-demos/getopts.sh
@@ -1,5 +1,22 @@
 # TODO: "lift" the structure of getopts
 
-
+while getopts ":n:v" opt; do
+  case $opt in
+    n)
+      NAME="$OPTARG"
+      ;;
+    v)
+      VERBOSE=true
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+  esac
+done
 # Design it with intent
 # Reviting either file maker OR the awk seeks from 2/16
