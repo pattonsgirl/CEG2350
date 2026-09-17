75692eaca54bd5fb777bc2cad76b79cebdbbe946
Kayleigh Duncan <pattonsgirl@gmail.com>
2026-09-17 13:26:25 +0000

    outlining make-a-long script

---
+++ scripts/file-create.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# function help: print a menu explaining what the script does and expected args
+
+# function make_files: take number of files as arg, create that many files in format i_file.txt
+
+# note: fuctions are being declared BEFORE we get to main.  You can make main to make this less confusing
+#
+# main()?
+
+# get a directory name
+
+# check if exits, create if not
+
+# confirm directory files will be made in - allow user to proceed or abort
+
+# get number of files to create
+
+# loop to make that many files - for or while?
+
